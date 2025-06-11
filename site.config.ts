c8a91de9ce1b497290e909fbf67d9b37321e1cff
Fuyun <3404889047@qq.com>
2025-06-12 07:58:52 +0800

    first commit

---
+++ site.config.ts
@@ -0,0 +1,119 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+  url: 'https://valaxy.site/',
+  lang: 'zh-CN',
+  title: '浮芸的云端',
+  subtitle: 'Fuyun-blog',
+  author: {
+    name: '浮芸',
+    avatar: '头像地址',
+     status:
+    {
+      emoji: '😐',
+      message: '真不想上学'
+  }
+  },
+  description: '编辑博客还是太难了',
+  favicon: '站点图标地址',
+  social: [
+    {
+      name: 'RSS',
+      link: '/atom.xml',
+      icon: 'i-ri-rss-line',
+      color: 'orange',
+    },
+    {
+      name: 'QQ  3404889047',
+      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
+      icon: 'i-ri-qq-line',
+      color: '#12B7F5',
+    },
+    {
+      name: 'GitHub',
+      link: 'https://bgithub.xyz/FuyunTechnology',
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    {
+      name: '哔哩哔哩',
+      link: 'https://space.bilibili.com/1579790',
+      icon: 'i-ri-bilibili-line',
+      color: '#FF8EB3',
+    },
+    {
+      name: 'Telegram Channel',
+      link: 'https://t.me/elpsycn',
+      icon: 'i-ri-telegram-line',
+      color: '#0088CC',
+    },
+    {
+      name: 'E-Mail',
+      link: 'mailto:me@yunyoujun.cn',
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+    {
+      name: 'Travelling',
+      link: 'https://www.travellings.cn/go.html',
+      icon: 'i-ri-train-line',
+      color: 'var(--va-c-text)',
+    },
+  ],
+
+    search: {
+    enable: true,
+    type: 'engine',
+  },
+
+  encrypt: {
+    // 开启加密，默认关闭
+    enable: true
+    // algorithm
+    // iv
+    // salt
+  },
+
+mediumZoom: { enable: true },
+
+  /**
+   * 开启阅读统计
+   */
+  statistics: {  //显示阅读信息
+    enable: true,
+    readTime: {
+      /**
+       * 阅读速度
+       */
+      speed: {
+        cn: 300,
+        en: 200,
+      },
+    },
+  },
+
+  sponsor: {
+    enable: true,
+    title: '看在收集了这么多软件的份上,给点钱吧',
+    methods: [
+      {
+        name: '支付宝',
+        url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
+        color: '#00A3EE',
+        icon: 'i-ri-alipay-line',
+      },
+      {
+        name: 'QQ 支付',
+        url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
+        color: '#12B7F5',
+        icon: 'i-ri-qq-line',
+      },
+      {
+        name: '微信支付',
+        url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
+        color: '#2DC100',
+        icon: 'i-ri-wechat-pay-line',
+      },
+    ],
+  },
+})
---
+++ valaxy.config.ts
@@ -0,0 +1,86 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+import { addonLightGallery } from 'valaxy-addon-lightgallery'
+import { addonComponents } from 'valaxy-addon-components'
+import { addonFace } from 'valaxy-addon-face'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '浮芸的云端',
+    },
+
+        bg_image: {
+      enable: true,
+      url: '/photo/bg/atri.webp',
+      dark: '/photo/bg/bg.webp',
+      opacity: 0.7
+    },
+
+     colors: {
+      primary: '#39c5bb'
+    },
+
+    pages: [
+      {
+        name: '我的小伙伴们',
+        url: '/links/',
+        icon: 'i-ri-genderless-line',
+        color: 'dodgerblue',
+      },
+      {
+        name: '喜欢的艺术家',
+        url: '/Artist/',
+        icon: 'i-ri-women-line',
+        color: 'hotpink',
+      },
+      {
+        name: '相册',
+        url: '/albums/',
+        icon: 'i-ri-gallery-line',
+        color: '#43abee',
+      },
+    ],
+
+	footer: {  //脚注
+      since: 2025,  //网站运行时间
+      icon: {
+        enable: true,  //是否启用
+        name: 'i-ri-heart-line',
+        animated: true,
+        color: '#d69b54',
+        url: 'https://yuumi.link',	//图标链接
+        title: '回到首页'		//鼠标悬停注释
+      },
+      beian: {
+        enable: true,
+        icp: '备案号:xxxx 真的备案了吗',	// 备案
+      },
+    },
+  },
+ 
+ addons: [
+    addonLightGallery(),   //添加相册插件
+    addonComponents(),  //添加通用组件
+    addonFace({
+      // 配置
+      defaultSuffix: 'png'
+    }),
+    // 如果不配置:'valaxy-addon-face'
+  ],
+  
+  unocss: { safelist },
+})
